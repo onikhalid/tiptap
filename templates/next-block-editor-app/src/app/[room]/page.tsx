76b22067b3c3a54a6815a4a3b489979529e5c6d9
Oni Khalid Ayòbámi <onikhalidayo@gmail.com>
2025-02-23 11:13:33 +0100

    update

--- templates/next-block-editor-app/src/app/[room]/page.tsx
+++ templates/next-block-editor-app/src/app/[room]/page.tsx
@@ -6,6 +6,7 @@ import { useSearchParams } from 'next/navigation'
 import { useCallback, useEffect, useLayoutEffect, useMemo, useState } from 'react'
 import { Doc as YDoc } from 'yjs'
 
+
 import { BlockEditor } from '@/components/BlockEditor'
 import { createPortal } from 'react-dom'
 import { Surface } from '@/components/ui/Surface'
